8462d377c4f271f44452638a7d36a98a495f719c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-07 01:07:23 +0900

    import modules

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next'
 import { LogoutIcon, DocumentTextIcon } from '@heroicons/react/solid'
+import { GetStaticProps } from 'next'
 import { supabase } from '../utils/supabase'
 import { Layout } from '../components/Layout'
+import { NoteForm } from '../components/NoteForm'
+import { NoteItem } from '../components/NoteItem'
+import { Note } from '../types/types'
 
 const Notes: NextPage = () => {
   const signOut = () => {
