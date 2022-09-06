c44aae870f6f42bc5b4e9e4039fb15c35906f416
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-09-07 00:41:43 +0900

    create NoteForm component

--- components/NoteForm.tsx
+++ components/NoteForm.tsx
@@ -26,5 +26,30 @@ export const NoteForm: React.FC = () => {
     }
   }
 
-  return <div>NoteForm</div>
+  if (createNoteMutation.isLoading || updateNoteMutation.isLoading)
+    return <Spinner />
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <input
+          type="text"
+          className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+          placeholder="title"
+          value={editedNote.title}
+          onChange={(e) => update({ ...editedNote, title: e.target.value })}
+        />
+      </div>
+      <div>
+        <textarea
+          cols={50}
+          rows={10}
+          className="my-2 rounded border border-gray-300 px-3 py-2 text-sm placeholder-gray-500 focus:border-indigo-500 focus:outline-none"
+          placeholder="content"
+          value={editedNote.content}
+          onChange={(e) => update({ ...editedNote, content: e.target.value })}
+        />
+      </div>
+    </form>
+  )
 }
